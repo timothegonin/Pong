6b7bacebf861dc6da8b57ec29cb7fc8729abff16
TimotheGonin <gonin.timothe@gmail.com>
2023-04-23 19:12:46 +0200

    ball on middle

--- main.lua
+++ main.lua
@@ -5,6 +5,13 @@ pad.height = 80
 pad.x = 0
 pad.y = 0
 
+--BALL
+ball = {}
+ball.width = 20
+ball.height = 20
+ball.x = love.graphics.getWidth() / 2 - ball.width / 2
+ball.y = love.graphics.getHeight() / 2 - ball.height / 2
+
 function love.load()
 end
 
@@ -22,4 +29,5 @@ end
 
 function love.draw()
   player1 = love.graphics.rectangle("fill", pad.x, pad.y, pad.width,pad.height)
+  theBall = love.graphics.rectangle("fill", ball.x, ball.y, ball.width, ball.height)
 end
